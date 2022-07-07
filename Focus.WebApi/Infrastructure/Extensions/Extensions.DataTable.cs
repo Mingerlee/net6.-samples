2408fa24b43c826d14943d5d93da70662f5e7422
ming <minger3027@163.com>
2022-07-07 16:20:29 +0800

    添加List、DataTable、DateTime扩展方法

---
+++ Focus.WebApi/Infrastructure/Extensions/Extensions.DataTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 将DataTable数据转换成List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<T> ToLists<T>(this DataTable dt) where T : class, new()
+        {
+            //创建一个属性的列表
+            List<PropertyInfo> prlist = new List<PropertyInfo>();
+            //获取TResult的类型实例  反射的入口
+            Type t = typeof(T);
+            //获得TResult 的所有的Public 属性 并找出TResult属性和DataTable的列名称相同的属性(PropertyInfo) 并加入到属性列表
+            Array.ForEach<PropertyInfo>(t.GetProperties(), p => { if (dt.Columns.IndexOf(p.Name) != -1) prlist.Add(p); });
+            //创建返回的集合
+            List<T> oblist = new List<T>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                //创建TResult的实例
+                T ob = new T();
+                //找到对应的数据  并赋值
+                prlist.ForEach(p => { if (row[p.Name] != DBNull.Value) p.SetValue(ob, row[p.Name], null); });
+                //放入到返回的集合中.
+                oblist.Add(ob);
+            }
+            return oblist;
+        }
+    }
+}
---
+++ Focus.WebApi/Infrastructure/Extensions/Extensions.List.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// List分页
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_list"></param>
+        /// <param name="PageIndex">当前页</param>
+        /// <param name="PageSize">一页显示数量</param>
+        /// <returns></returns>
+        public static List<T> SplitList<T>(this List<T> _list, int PageIndex, int PageSize)
+        {
+            int _PageIndex = PageIndex == 0 ? 1 : PageIndex;
+            int _PageSize = PageSize == 0 ? 20 : PageSize;
+            int PageConut = (int)Math.Ceiling(Convert.ToDecimal(_list.Count) / _PageSize);
+            if (PageConut >= _PageIndex)
+            {
+                List<T> list = new List<T>();
+                list = _list.Skip((_PageIndex - 1) * _PageSize).Take(_PageSize).ToList();
+                return list;
+            }
+            else
+                return _list;
+        }
+        /// <summary>
+        /// 将List数据转换为DataTable
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable(this IList list)
+        {
+            DataTable result = new DataTable();
+            if (list.Count > 0)
+            {
+                PropertyInfo[] propertys = list[0].GetType().GetProperties();
+                foreach (PropertyInfo pi in propertys)
+                {
+                    //获取类型
+                    Type colType = pi.PropertyType;
+                    //当类型为Nullable<>时
+                    if ((colType.IsGenericType) && (colType.GetGenericTypeDefinition() == typeof(Nullable<>)))
+                    {
+                        colType = colType.GetGenericArguments()[0];
+                    }
+                    result.Columns.Add(pi.Name, colType);
+                }
+                for (int i = 0; i < list.Count; i++)
+                {
+                    ArrayList tempList = new ArrayList();
+                    foreach (PropertyInfo pi in propertys)
+                    {
+                        object obj = pi.GetValue(list[i], null);
+                        tempList.Add(obj);
+                    }
+                    object[] array = tempList.ToArray();
+                    result.LoadDataRow(array, true);
+                }
+            }
+            return result;
+        }
+
+    }
+}
